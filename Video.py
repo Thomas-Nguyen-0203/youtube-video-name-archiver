63a58896c3a757ed6da4858c735fdd08c383e04a
Thomas Nguyen <thomasng5000@gmail.com>
2022-12-30 19:57:04 +1100

    added repr

--- Video.py
+++ Video.py
@@ -1,17 +1,21 @@
 import typing
 
 YOUTUBE_VIDEO_PREFIX = "youtube.com/watch?v="
+
 class Video:
 	def __init__(self, name: str, channel: str, id: str):
 		self.name: str = name
 		self.channel: str = channel
 		self.link: str = YOUTUBE_VIDEO_PREFIX + id
 
-	def get_name() -> str:
+	def get_name(self) -> str:
 		return self.name
 
-	def get_channel() -> str:
+	def get_channel(self) -> str:
 		return self.channel
 
-	def get_link() -> str:
+	def get_link(self) -> str:
 		return self.link
+	
+	def __repr__(self) -> str:
+		return self.name + "," + self.channel + "," + self.link
