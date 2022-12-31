58469cda7441d9ca79735514e91cd24ff3183f3e
Thomas Nguyen <thomasng5000@gmail.com>
2023-01-01 01:19:45 +1100

    Added some more constant variables that will help

--- configuration.py
+++ configuration.py
@@ -1,3 +1,16 @@
+import re
+from typing import *
+
 # This is an old key and it was already deprecated.
 # You just need to generate your own key then substitute it in.
-API_KEY = "AIzaSyB_5_NDZPeq7P3JbE0vUCt8UoXWBaBBEYo"
+API_KEY: str = "AIzaSyB_5_NDZPeq7P3JbE0vUCt8UoXWBaBBEYo"
+
+API_URL: str = "https://www.googleapis.com/youtube/v3/playlistItems"
+
+_PLAYLIST_URL_REGEX_STR: str = "https://(?:www\\.)?youtube\\.com/playlist\\?list=([a-zA-Z0-9_\\-]+)"
+
+PLAYLIST_REGEX: re = re.compile(_PLAYLIST_URL_REGEX_STR)
+
+PARAMS: dict[str,str] = {
+
+}
