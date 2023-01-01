bf23547300ce2e30dce3cb64ec588eaf1601d2fc
Thomas Nguyen <thomasng5000@gmail.com>
2023-01-02 04:09:51 +1100

    Added one more method to make json dump easier

--- Video.py
+++ Video.py
@@ -1,4 +1,4 @@
-import typing
+from typing import *
 
 YOUTUBE_VIDEO_PREFIX = "youtube.com/watch?v="
 
@@ -6,7 +6,8 @@ class Video:
 	def __init__(self, name: str, channel: str, id: str):
 		self.name: str = name
 		self.channel: str = channel
-		self.link: str = YOUTUBE_VIDEO_PREFIX + id
+		self.id = id
+		self.link: str = YOUTUBE_VIDEO_PREFIX + self.id
 
 	def get_name(self) -> str:
 		return self.name
@@ -16,6 +17,43 @@ class Video:
 
 	def get_link(self) -> str:
 		return self.link
+
+	def get_id(self) -> str:
+		return self.id
+
+	def construct_json_obj(self):
+		'''
+        This method constructs the dictionary which is the representation of 
+        the video and return it.
+
+        Returns:
+            The dictionary representation of the JSON object of the video.
+        '''
+		json_obj = {
+			"id": self.id,
+			"name": self.name,
+			"channel": self.channel,
+			"link": self.link,
+		}
+
+		return json_obj
 	
 	def __repr__(self) -> str:
 		return self.name + "," + self.channel + "," + self.link
+
+	def get_list(self) -> List[str]:
+		return [self.name, self.channel, self.link, self.id]
+
+	def __eq__(self, other: 'Video'):
+		
+		# Youtube video's ids are unique so comparing it should be the most 
+		# logical way
+		if not (isinstance(other, Video)):
+
+			return False
+
+		if (other.id == self.id):
+			return True
+
+		return False
+		
